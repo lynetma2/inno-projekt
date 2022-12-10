b635c42c79e1f2248cc4169804081f8cec1db7ab
lynetma2 <lynetma2@users.noreply.github.com>
2022-12-10 21:42:05 +0100

    app frontend udseende done

--- frontend/app/lib/main.dart
+++ frontend/app/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:app/resultPage.dart';
-import 'package:app/testChart.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_commons/google_mlkit_commons.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
@@ -16,12 +15,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Paint-O-Meter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Los Colorum'),
     );
   }
 }
@@ -38,13 +37,13 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   void _findtext() async {
-    // final ImagePicker _picker = ImagePicker();
-    // final XFile? image = await _picker.pickImage(source: ImageSource.camera);
-    // debugPrint(image!.path);
-    // final inputImage = InputImage.fromFilePath(image.path);
-    // final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
-    // final RecognizedText recognizedText = await textRecognizer.processImage(inputImage);
-    // debugPrint(recognizedText.text);
+    //final ImagePicker _picker = ImagePicker();
+    //final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+    //debugPrint(image!.path);
+    //final inputImage = InputImage.fromFilePath(image.path);
+    //final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+    //final RecognizedText recognizedText = await textRecognizer.processImage(inputImage);
+    //debugPrint(recognizedText.text);
     _switchPage();
   }
 
@@ -57,21 +56,56 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("App Navn"),
+        title: Text("Paint-O-Meter"),
         centerTitle: true,
       ),
       body: SingleChildScrollView(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            ElevatedButton(
-              child: Text("Search"),
-              onPressed: _findtext,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: _findtext,
+                child: SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const [
+                      Icon(Icons.camera_alt),
+                      Text("Søg"),
+                    ],
+                  ),
+                ),
+              ),
             ),
-            SizedBox(height: 300, child: TestChart.withSampleData()),
+            SizedBox(height: 16,),
+            const Text("Historik", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+            ListView(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(8),
+              children: [
+                productWidget("Flügger Facade Beton - Betonmaling", "https://assets.flugger.dk/static/ir/789878/24408_Facade%20Beton_07_0,75L_FACADE%20BET%20(1).png?width=500&quality=80&format=webp&rmode=Pad", 1, 60),
+                productWidget("Flügger Facade Beton - Betonmaling", "https://assets.flugger.dk/static/ir/789878/24408_Facade%20Beton_07_0,75L_FACADE%20BET%20(1).png?width=500&quality=80&format=webp&rmode=Pad", 1, 60),
+                productWidget("Flügger Facade Beton - Betonmaling", "https://assets.flugger.dk/static/ir/789878/24408_Facade%20Beton_07_0,75L_FACADE%20BET%20(1).png?width=500&quality=80&format=webp&rmode=Pad", 1, 60),
+              ],
+            )
           ],
         ),
       ),
     );
   }
+}
+
+Widget productWidget(String productName, String imgUrl, int dryTime, int relativeHumidity) {
+  return
+    Card(
+      child: ListTile(
+        title: Text(productName),
+        subtitle: Text("Tørretid: $dryTime\n Fugt < $relativeHumidity"),
+        trailing: Image.network(imgUrl),
+      ),
+    );
 }
